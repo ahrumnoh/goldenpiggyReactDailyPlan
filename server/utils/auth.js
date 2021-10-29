eb1dc132f5d5fcbcc481423745d93ba61b1b3142
ahrum noh <sydneyroseenglish@gmail.com>
2021-10-29 18:33:29 +1100

    server/seeders/seed

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = 'shh'; //previous secret ='mysecretssshhhhhhh'
 const expiration = '2h';
 
 module.exports = {
